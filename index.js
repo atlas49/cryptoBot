65775adf3da3b0fe182a4d0384a64ab325699970
Serhii <serhii.p@code-care.pro>
2023-09-18 18:19:37 +0300

    Fix token

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-import TelegramBot from 'node-telegram-bot-api';
-import env from 'dotenv';
-env.config();
+const TelegramBot = require('node-telegram-bot-api');
 
-console.log(process.env.TOKEN);
-const bot = new TelegramBot(process.env.TOKEN, { polling: true });
+const token = '6537097612:AAFLf_NsnT5ClUuoIc-5hbuvG0SyUPZX8Ew';
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  const chatId = msg.chat.id;
+  const resp = match[1];
+
+  bot.sendMessage(chatId, resp);
+});
 
 bot.on('message', (msg) => {
-  const {
-    chat: { id },
-  } = msg;
+  const chatId = msg.chat.id;
 
-  bot.sendMessage(id, 'Pong');
+  bot.sendMessage(chatId, 'Received your message');
 });
